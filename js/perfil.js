20fb431696bbf53ee868502666f9602e15eb84c2
iamfernanda <fda.isla@gmail.com>
2018-07-19 14:32:03 -0400

    html, css,perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,3 +1,11 @@
+//funcion para edicion de nombre de usuario//
+function uno() {
+    document.getElementById("boxPerfil").style.visibility = "visible";
+   }
+
+
+
+
 function previewImg(file) {
     var reader = new FileReader();
     reader.onload = function() {
